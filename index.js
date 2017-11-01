63e5992c6b953d652952cecb93468897ae8e5e29
jonschlinkert <jon.schlinkert@sellside.com>
2017-11-01 02:31:30 -0400

    run update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * is-data-descriptor <https://github.com/jonschlinkert/is-data-descriptor>
  *
- * Copyright (c) 2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2015-2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
