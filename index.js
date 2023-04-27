c18a23640c00f32fca39112381b5cabdaa6a9a55
Jordan Harband <ljharb@gmail.com>
2023-04-27 11:40:11 -0700

    [eslint] cleanup

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 'use strict';
 
 const hasOwn = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key);
-const isObject = val => {
-  return val !== null && typeof val === 'object' && !Array.isArray(val);
-};
+const isObject = (val) => val !== null && typeof val === 'object' && !Array.isArray(val);
 
 const isDescriptor = (obj, key) => {
-  if (!isObject(obj)) return false;
-  let desc = key ? Object.getOwnPropertyDescriptor(obj, key) : obj;
-  if (isObject(desc)) {
-    let booleans = ['configurable', 'enumerable', 'writable'];
-    if (!hasOwn(desc, 'value') || hasOwn(desc, 'get') || hasOwn(desc, 'set')) {
-      return false;
-    }
-    for (let key of Object.keys(desc)) {
-      if (booleans.includes(key) && typeof desc[key] !== 'boolean') {
-        return false;
-      }
-      if (!booleans.includes(key) && key !== 'value') {
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;
+	if (!isObject(obj)) {
+		return false;
+	}
+	const desc = key ? Object.getOwnPropertyDescriptor(obj, key) : obj;
+	if (isObject(desc)) {
+		const booleans = [
+			'configurable', 'enumerable', 'writable',
+		];
+		if (!hasOwn(desc, 'value') || hasOwn(desc, 'get') || hasOwn(desc, 'set')) {
+			return false;
+		}
+		for (const descKey of Object.keys(desc)) {
+			if (booleans.includes(descKey) && typeof desc[descKey] !== 'boolean') {
+				return false;
+			}
+			if (!booleans.includes(descKey) && descKey !== 'value') {
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
 };
 
 module.exports = isDescriptor;
