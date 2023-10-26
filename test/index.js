70540e5449ef3239051d4e40dce8a5a1978d1634
Jordan Harband <ljharb@gmail.com>
2023-10-25 23:06:09 -0700

    [Tests] switch to tape

--- test/index.js
+++ test/index.js
@@ -1,55 +1,64 @@
 'use strict';
 
-require('mocha');
-var assert = require('assert');
+var test = require('tape');
 var isDescriptor = require('../');
 var noop = function () {};
 
-describe('isDescriptor', function () {
-	describe('value type', function () {
-		it('should be false when not an object:', function () {
-			assert(!isDescriptor('a'));
-			assert(!isDescriptor(null));
-			assert(!isDescriptor([]));
-		});
+test('isDescriptor', function (t) {
+	t.test('value type', function (st) {
+		st.notOk(isDescriptor('a'), 'string is not a descriptor');
+		st.notOk(isDescriptor(null), 'null is not a descriptor');
+		st.notOk(isDescriptor([]), 'Array is not a descriptor');
+
+		st.end();
 	});
 
-	describe('data descriptor:', function () {
-		it('should not be false when the object has invalid properties:', function () {
-			assert(isDescriptor({ value: 'foo', bar: 'baz' }));
-			assert(isDescriptor({ value: 'foo', bar: 'baz' }));
-			assert(isDescriptor({ value: 'foo', get: noop }));
-			assert(isDescriptor({ get: noop, value: noop }));
-		});
+	t.test('should not be false when the object has invalid properties:', function (st) {
+		st.ok(isDescriptor({ value: 'foo', bar: 'baz' }));
+		st.ok(isDescriptor({ value: 'foo', bar: 'baz' }));
+		st.ok(isDescriptor({ value: 'foo', get: noop }));
+		st.ok(isDescriptor({ get: noop, value: noop }));
 
-		it('should be true when the object has valid data-descriptor properties', function () {
-			assert(isDescriptor({ value: 'foo' }));
-			assert(isDescriptor({ value: noop }));
-		});
-
-		it('should be false when valid properties are invalid types', function () {
-			assert(!isDescriptor({ value: 'foo', enumerable: 'foo' }));
-			assert(!isDescriptor({ value: 'foo', configurable: 'foo' }));
-			assert(!isDescriptor({ value: 'foo', writable: 'foo' }));
-		});
-
-		it('should be true when a value is a valid data descriptor', function () {
-			assert(isDescriptor({ value: 'foo' }));
-			assert(isDescriptor({ writable: true }));
-			assert(isDescriptor({ value: 'foo', get: 'foo' }));
-		});
-
-		it('should be false when the value is not a valid descriptor', function () {
-			assert(!isDescriptor('foo'));
-			assert(!isDescriptor({}));
-			assert(!isDescriptor({ configurable: true }));
-			assert(!isDescriptor({ enumerable: true }));
-			assert(!isDescriptor({
-				get: undefined,
-				set: undefined,
-				enumerable: true,
-				configurable: true,
-			}));
-		});
+		st.end();
 	});
+
+	t.test('should be true when the object has valid data-descriptor properties', function (st) {
+		st.ok(isDescriptor({ value: 'foo' }));
+		st.ok(isDescriptor({ value: noop }));
+
+		st.end();
+	});
+
+	t.test('should be false when valid properties are invalid types', function (st) {
+		st.notOk(isDescriptor({ value: 'foo', enumerable: 'foo' }));
+		st.notOk(isDescriptor({ value: 'foo', configurable: 'foo' }));
+		st.notOk(isDescriptor({ value: 'foo', writable: 'foo' }));
+
+		st.end();
+	});
+
+	t.test('should be true when a value is a valid data descriptor', function (st) {
+		st.ok(isDescriptor({ value: 'foo' }));
+		st.ok(isDescriptor({ writable: true }));
+		st.ok(isDescriptor({ value: 'foo', get: 'foo' }));
+
+		st.end();
+	});
+
+	t.test('should be false when the value is not a valid descriptor', function (st) {
+		st.notOk(isDescriptor('foo'));
+		st.notOk(isDescriptor({}));
+		st.notOk(isDescriptor({ configurable: true }));
+		st.notOk(isDescriptor({ enumerable: true }));
+		st.notOk(isDescriptor({
+			get: undefined,
+			set: undefined,
+			enumerable: true,
+			configurable: true,
+		}));
+
+		st.end();
+	});
+
+	t.end();
 });
